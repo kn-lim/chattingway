f8d41b346cf56fe79ce63735725cfd8273fed9f8
Kevin Lim <17999246+kn-lim@users.noreply.github.com>
2025-11-18 00:14:38 -0800

    chore(deps): update github actions (major) (#7)
    * chore(deps): update github actions
    
    * fix: lint

--- mcstatus/mcstatus.go
+++ mcstatus/mcstatus.go
@@ -24,7 +24,7 @@ func GetMCStatus(serverURL string) (bool, int, error) {
 	if err != nil {
 		return false, 0, err
 	}
-	defer response.Body.Close()
+	defer func() { _ = response.Body.Close() }()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
